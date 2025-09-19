51ce556ecc036daf5970db348f25489420613c17
HenryWall85 <Henrywall85@gmail.com>
2025-09-18 23:41:12 -0600

    Sidebar v2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,21 @@ import './Sidebar.css'
 function Sidebar(){
     return(
         <div className='sidebar'>
-            <div className='sidebar-header'>
-                <ul className="sidebar-nav">
-                    <li className="nav-item"><a href="dashboard" className="nav-link">Dashboard</a></li>
-                    <li className="nav-item"><a href="planner" className="nav-link">Planner</a></li>
-                    <li className="nav-item"><a href="transactions" className="nav-link">Transactions</a></li>
-                </ul>
+            <div className='name-card'>
+                <div className='name-card-badge'>
+                    <p>HW</p>
+                </div>
+                <div className='name-card-userName'>
+                    <p>Henry Wall</p>
+                    <p className='name-card-logOut'>Sign Out</p>
+                </div>
             </div>
+            <span className='divider'></span>
+            <ul className="sidebar-nav">
+                <li className="nav-item"><a href="dashboard" className="nav-link">Dashboard</a></li>
+                <li className="nav-item"><a href="planner" className="nav-link">Planner</a></li>
+                <li className="nav-item"><a href="transactions" className="nav-link">Transactions</a></li>
+            </ul>
         </div>
     );
 }
