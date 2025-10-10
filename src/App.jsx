026e86d67faed52af5c4ea6b727a5c7592c4e594
HenryWall85 <Henrywall85@gmail.com>
2025-10-09 20:12:33 -0600

    Commit (Oct 09,2025)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
+import BudgetContainer from './components/BudgetContainer';
 import './App.css';
 
 function App(){
   return(
   <div className="app-container">
     <Sidebar />
+    
     <div className="main-content">
-      <Header />
+      <BudgetContainer/>
     </div>
   </div>
   );
---
+++ src/components/BudgetContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BudgetContainer.css'
+
+function BudgetContainer ({title}){
+    return(
+    <div className="Budget-Container">
+        <div className="container-actions">
+            {/* Placeholder for future actions like search or buttons */}
+        </div>
+    </div>
+    );
+}
+
+export default BudgetContainer;
